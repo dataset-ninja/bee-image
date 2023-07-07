a208da2a6ebd337236c6e4c758fb0935a651d9c1
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-07-07 09:19:37 +0300

    Implemented converter.

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,13 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import file_exists, get_file_name, get_file_size
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -53,21 +54,68 @@ def download_dataset(teamfiles_dir: str) -> str:
     return dataset_path
 
 
-
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    dataset_path = "/home/iwatkot/supervisely/ninja-datasets/bee-image"
+    batch_size = 30
+    ds_name = "ds"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    images_folder = "images/images"
+    bboxes_folder = "labels 2/labels"
 
-    # ... some code here ...
+    def create_ann(image_path):
+        labels = []
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # return project
+        mask_name = get_file_name(image_path) + ".txt"
+        mask_path = os.path.join(bboxes_path, mask_name)
+        if file_exists(mask_path):
+            with open(mask_path) as f:
+                content = f.read().split("\n")
 
+                for curr_data in content:
+                    if len(curr_data) != 0:
+                        ann_data = list(map(float, curr_data.split(" ")))
+                        left = int((ann_data[1] - ann_data[3] / 2) * img_wight)
+                        right = int((ann_data[1] + ann_data[3] / 2) * img_wight)
+                        top = int((ann_data[2] - ann_data[4] / 2) * img_height)
+                        bottom = int((ann_data[2] + ann_data[4] / 2) * img_height)
+                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+                        label = sly.Label(rectangle, obj_class)
+                        labels.append(label)
 
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    obj_class = sly.ObjClass("bee", sly.Rectangle)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[obj_class])
+    api.project.update_meta(project.id, meta.to_json())
+
+    images_path = os.path.join(dataset_path, images_folder)
+    bboxes_path = os.path.join(dataset_path, bboxes_folder)
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    images_names = os.listdir(images_path)
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+    for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+        images_pathes_batch = [
+            os.path.join(images_path, image_name) for image_name in img_names_batch
+        ]
+
+        img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns_batch)
+
+        progress.iters_done_report(len(img_names_batch))
+
+    return project
